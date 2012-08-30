7325bb043ce62e6a3ea744441e9db8a94614784d
Mumo Carlos <mumo.crls@gmail.com>
2012-08-29 23:59:13 -0500

    mobile page with slider

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -10,8 +10,8 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//
 //= require jquery
-//= require jquery_ujs
-//= require jQuery.radmenu
-//= require jgestures
-//= require radius_menu
+//= require mobile-bindings
+//= require jquery.mobile-1.2.0-alpha.1
+//= require jquery.pageslide
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Menus::Application.routes.draw do
   root to: 'home#index'
+  match 'mobile', to: 'mobile#index'
 end
